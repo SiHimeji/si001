0ca87907ffbc8b56e3152dd9484b88ebe85fee95
ChiharuFujishiro <si.himeji@gmail.com>
2025-08-09 09:57:27 +0900

    adin

---
+++ Addin/ExcelVB001/ExcelVB001/FormPdfPass.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormPdfPass
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button実行 = New System.Windows.Forms.Button()
+        Me.Buttonキャンセル = New System.Windows.Forms.Button()
+        Me.TextBoxFileName = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button検索 = New System.Windows.Forms.Button()
+        Me.TextBoxPass1 = New System.Windows.Forms.TextBox()
+        Me.TextBoxPass2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(24, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "ファイル"
+        '
+        'Button実行
+        '
+        Me.Button実行.Location = New System.Drawing.Point(218, 83)
+        Me.Button実行.Name = "Button実行"
+        Me.Button実行.Size = New System.Drawing.Size(75, 23)
+        Me.Button実行.TabIndex = 1
+        Me.Button実行.Text = "実行"
+        Me.Button実行.UseVisualStyleBackColor = True
+        '
+        'Buttonキャンセル
+        '
+        Me.Buttonキャンセル.Location = New System.Drawing.Point(400, 83)
+        Me.Buttonキャンセル.Name = "Buttonキャンセル"
+        Me.Buttonキャンセル.Size = New System.Drawing.Size(75, 23)
+        Me.Buttonキャンセル.TabIndex = 2
+        Me.Buttonキャンセル.Text = "キャンセル"
+        Me.Buttonキャンセル.UseVisualStyleBackColor = True
+        '
+        'TextBoxFileName
+        '
+        Me.TextBoxFileName.Location = New System.Drawing.Point(69, 10)
+        Me.TextBoxFileName.Name = "TextBoxFileName"
+        Me.TextBoxFileName.Size = New System.Drawing.Size(325, 19)
+        Me.TextBoxFileName.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(24, 40)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(82, 12)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "ユーザパスワード"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(24, 62)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(91, 12)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "オーナーパスワード"
+        '
+        'Button検索
+        '
+        Me.Button検索.Location = New System.Drawing.Point(400, 9)
+        Me.Button検索.Name = "Button検索"
+        Me.Button検索.Size = New System.Drawing.Size(75, 23)
+        Me.Button検索.TabIndex = 6
+        Me.Button検索.Text = "検索"
+        Me.Button検索.UseVisualStyleBackColor = True
+        '
+        'TextBoxPass1
+        '
+        Me.TextBoxPass1.Location = New System.Drawing.Point(126, 33)
+        Me.TextBoxPass1.Name = "TextBoxPass1"
+        Me.TextBoxPass1.Size = New System.Drawing.Size(167, 19)
+        Me.TextBoxPass1.TabIndex = 7
+        '
+        'TextBoxPass2
+        '
+        Me.TextBoxPass2.Location = New System.Drawing.Point(126, 58)
+        Me.TextBoxPass2.Name = "TextBoxPass2"
+        Me.TextBoxPass2.Size = New System.Drawing.Size(167, 19)
+        Me.TextBoxPass2.TabIndex = 8
+        '
+        'FormPdfPass
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(480, 137)
+        Me.Controls.Add(Me.TextBoxPass2)
+        Me.Controls.Add(Me.TextBoxPass1)
+        Me.Controls.Add(Me.Button検索)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBoxFileName)
+        Me.Controls.Add(Me.Buttonキャンセル)
+        Me.Controls.Add(Me.Button実行)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "FormPdfPass"
+        Me.Text = "パスワード設定"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Windows.Forms.Label
+    Friend WithEvents Button実行 As Windows.Forms.Button
+    Friend WithEvents Buttonキャンセル As Windows.Forms.Button
+    Friend WithEvents TextBoxFileName As Windows.Forms.TextBox
+    Friend WithEvents Label2 As Windows.Forms.Label
+    Friend WithEvents Label3 As Windows.Forms.Label
+    Friend WithEvents Button検索 As Windows.Forms.Button
+    Friend WithEvents TextBoxPass1 As Windows.Forms.TextBox
+    Friend WithEvents TextBoxPass2 As Windows.Forms.TextBox
+End Class
---
+++ Addin/ExcelVB001/ExcelVB001/FormPdfPass.vb
@@ -0,0 +1,128 @@
+﻿Imports System.IO
+Imports iTextSharp.text.pdf
+Imports System.Windows.Forms
+Imports iTextSharp.text
+Imports Microsoft.Office.Tools.Ribbon
+Imports iTextSharp
+'Imports iTextSharp.text.pdf
+Imports System.Diagnostics
+
+Public Class FormPdfPass
+    Private Sub Button実行_Click(sender As Object, e As EventArgs) Handles Button実行.Click
+
+        Dim ret As Boolean = SetPdfPassword(TextBoxFileName.Text, TextBoxPass1.Text, TextBoxPass2.Text)
+        If ret Then
+            MessageBox.Show("パスワードを設定しました")
+        End If
+
+        Me.Close()
+    End Sub
+    '/// <summary>
+    '/// PDFファイルへパスワードを設定する
+    '/// </summary>
+    '/// <param name="sPdfPath">
+    '/// パスワードを設定するPDF
+    '/// </param>
+    '/// <param name="sPass1">
+    '/// ユーザパスワード
+    '/// </param>
+    '/// <param name="sPass2">
+    '/// オーナーパスワード
+    '/// </param>
+    '/// <returns>
+    '/// true:正常
+    '/// false:異常
+    '/// </returns> 
+    Private Function SetPdfPassword(sPdfPath As String, sPass1 As String, sPass2 As String) As Boolean
+        Dim bRet As Boolean = True
+        Dim dtNow As DateTime = DateTime.Now
+
+        Dim sLoclTm As String = String.Format("{0}{1:D2}{2:D2}{3:D2}{4:D2}{5:D2}{6}",
+                                            dtNow.Year,
+                                            dtNow.Month,
+                                            dtNow.Day,
+                                            dtNow.Hour,
+                                            dtNow.Minute,
+                                            dtNow.Second,
+                                            dtNow.Millisecond)
+
+        Dim sFilePath_PW As String = sPdfPath + "_" + sLoclTm + "_temp_pw.pdf"
+        Dim objPdfReader As PdfReader = Nothing
+
+        Try
+
+            Dim byteUSER As Byte() = Encoding.ASCII.GetBytes(sPass1) '// ユーザパスワード
+            Dim byteOWNER As Byte() = Encoding.ASCII.GetBytes(sPass2) '// オーナーパスワー
+
+            If File.Exists(sPdfPath) = False Then
+                Return False
+            End If
+            objPdfReader = New PdfReader(sPdfPath)
+
+            Using objStreamOutput As Stream = New FileStream(sFilePath_PW,
+                                                                 FileMode.Create,
+                                                                 FileAccess.Write,
+                                                                 FileShare.None)
+
+                PdfEncryptor.Encrypt(objPdfReader,
+                                     objStreamOutput,
+                                     byteUSER,
+                                      byteOWNER,
+                                      PdfWriter.ALLOW_COPY Or PdfWriter.ALLOW_PRINTING,
+                                      PdfWriter.STRENGTH128BITS)
+
+                objPdfReader.Close()
+                objPdfReader = Nothing
+                File.Delete(sPdfPath)
+                File.Move(sFilePath_PW, sPdfPath)
+
+            End Using
+
+        Catch ex As Exception
+            bRet = False
+        Finally
+            objPdfReader.Close()
+        End Try
+
+        Return bRet
+    End Function
+
+    Private Sub Buttonキャンセル_Click(sender As Object, e As EventArgs) Handles Buttonキャンセル.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button検索_Click(sender As Object, e As EventArgs) Handles Button検索.Click
+        'OpenFileDialogクラスのインスタンスを作成
+        Dim ofd As New OpenFileDialog()
+
+        'はじめのファイル名を指定する
+        'はじめに「ファイル名」で表示される文字列を指定する
+        ofd.FileName = "default.pdf"
+        'はじめに表示されるフォルダを指定する
+        '指定しない（空の文字列）の時は、現在のディレクトリが表示される
+        ofd.InitialDirectory = "C:\"
+        '[ファイルの種類]に表示される選択肢を指定する
+        '指定しないとすべてのファイルが表示される
+        ofd.Filter = "PDFファイル(*.pdf;*.pdf)|*.pdf;*.pdf|すべてのファイル(*.*)|*.*"
+        '[ファイルの種類]ではじめに選択されるものを指定する
+        '2番目の「すべてのファイル」が選択されているようにする
+        ofd.FilterIndex = 2
+        'タイトルを設定する
+        ofd.Title = "開くファイルを選択してください"
+        'ダイアログボックスを閉じる前に現在のディレクトリを復元するようにする
+        ofd.RestoreDirectory = True
+        '存在しないファイルの名前が指定されたとき警告を表示する
+        'デフォルトでTrueなので指定する必要はない
+        ofd.CheckFileExists = True
+        '存在しないパスが指定されたとき警告を表示する
+        'デフォルトでTrueなので指定する必要はない
+        ofd.CheckPathExists = True
+
+        'ダイアログを表示する
+        If ofd.ShowDialog() = DialogResult.OK Then
+            'OKボタンがクリックされたとき、選択されたファイル名を表示する
+            TextBoxFileName.Text = ofd.FileName
+        End If
+
+    End Sub
+End Class
