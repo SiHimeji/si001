cfc7171ead9ba5ef6c514a2a58027f24f49a6b97
chiharuFujishiro <boxster986c@gmail.com>
2025-08-10 09:20:45 +0900

    DEL

--- python/dir.py
+++
@@ -1,8 +0,0 @@
-import os
-path ="d:\\"
-dirs =os.listdir(path)
-for dirname in dirs:
-    print(dirname)
-#     'marged_file = "D:\\" + dirname + ".pdf"
-#    'with open (marged_file,"wb") as f:
-#    'pdf_wrier.write(f)
